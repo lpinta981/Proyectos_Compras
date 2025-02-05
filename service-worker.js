ac214604f3fba302f7a940a6a51ccd5d78a423b0
lpinta981 <108894335+lpinta981@users.noreply.github.com>
2025-02-05 14:35:34 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,9 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '/pwaluispinta/',
-        '/pwaluispinta/index.html',
-        '/pwaluispinta/manifest.json',
+        '/proyectosapp/',
+        '/proyectosapp/index.html',
+        '/proyectosapp/manifest.json',
         'https://static.vecteezy.com/system/resources/previews/008/852/473/non_2x/shopping-bags-and-paper-shopping-list-options-or-reminders-isolated-on-a-white-background-3d-rendering-free-photo.jpg'
       ]);
     })
