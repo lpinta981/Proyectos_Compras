85a1bdf4eb9bae32484003685be9c87cdb179fa8
lpinta981 <108894335+lpinta981@users.noreply.github.com>
2025-02-05 14:32:16 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/pwaluispinta/',
+        '/pwaluispinta/index.html',
+        '/pwaluispinta/manifest.json',
+        'https://static.vecteezy.com/system/resources/previews/008/852/473/non_2x/shopping-bags-and-paper-shopping-list-options-or-reminders-isolated-on-a-white-background-3d-rendering-free-photo.jpg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
